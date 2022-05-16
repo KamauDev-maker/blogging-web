143eceebc4bb2e9705ba5db9d1bf04156c38d2d3
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-16 19:00:01 +0300

    initializing the flask email extension in the app

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:2021@localhost/blogpost'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 class ProdConfig(Config):
     
     pass   
