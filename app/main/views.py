3d842816ab5090071fec692cccc5ba03c59a5c82
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-16 12:54:22 +0300

    adding the get post methods

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template,redirect,url
+from flask import render_template,redirect,url_for,abort
 from . import main
+from . forms import PostForm, CommentForm, CategoryForm,UpdateProfile
+from ..requests import get_quote
 
 @main.route('/')
 def index():
@@ -12,44 +14,92 @@ def index():
     title = 'Blog Post'
     return render_template('index.html',all_posts= all_posts, categories = all_category, title=title,quote=quote)
 
-@main.route('/category/new-post/<int:id>')
+@main.route('/category/new-post/<int:id>',methods = ['POST','GET'])
 def new_post(id):
-    
+    form = PostForm()
     category = PostCategory.query.filter_by(id=id).first()
+    
+    if category is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        content = form.content.data
+        new_post= Post(content=content, category_id = category.id, user_id = current_user.id)
+        new_post.save_post()
+        return redirect(url_for('.category', id=category.id))
 
     
-    return render_template('new_post.html')
+    return render_template('new_post.html',post_form = form, category = category)
 
 @main.route('/categories/<int:id>')
 def category(id):
     
     category = PostCategory.query.filter_by(id)
+    if category is None:
+        abort(404)
     
     posts = Post.get_posts(id)
     return render_template('category.html',posts = posts, category=category)
     
-@main.route('/add/category')
+@main.route('/add/category',methods = ['GET','POST'])
+
 def new_category():
+    form = CategoryForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = PostCategory(name = name)
+        new_category.save_category()
+
+        return redirect(url_for('.index'))
+
     
     title = 'New Category'
     
-    return render_template('new_category.html',title = title)
+    return render_template('new_category.html',title = title, category_form = form)
 
-@main.route('/view-pitch/<int:id>')
+@main.route('/view-post/<int:id>',methods = ['GET','POST'])
 def view_pitch(id):
     
     all_category = PostCategory.get_categories()
     posts = Post.query.get(id)
     
-    return render_template('view-pitch.html')
+    if posts is None:
+        abort(404)
+    
+    comment = Comments.get_comments(id)
+    count_likes = Votes.query.filter_by(posts_id=id, vote=1).all()
+    count_dislikes = Votes.query.filter_by(posts_id=id, vote=2).all()
+    return render_template('view-post.html', posts = posts, comment = comment, count_likes=len(count_likes), count_dislikes=len(count_dislikes), category_id = id, categories=all_category)
+    
 
-@main.route('/write_comment/<int:id>')
+@main.route('/write_comment/<int:id>', methods = ['GET','POST'])
 def post_comment(id):
     
+    form = CommentForm()
     title = 'post comment'
     posts = Post.query.filter_by(id=id).first()
     
-    return render_template('post_comment.html', title = title)
+    if pitches is None:
+         abort(404)
+
+    if form.validate_on_submit():
+        opinion = form.opinion.data
+        new_comment = Comments(opinion = opinion, user_id = current_user.id, post_id = post.id)
+        new_comment.save_comment()
+        return redirect(url_for('.view_post', id = posts.id))
+    
+    return render_template('post_comment.html', title = title, comment_form = form)
+
+@main.route("/post/<int:id>/<int:comment_id>/delete")
+def delete_comment(id, comment_id):
+    post = Post.query.filter_by(id = id).first()
+    comment = Comment.query.filter_by(id = comment_id).first()
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for("main.post", id = post.id))
+
+
 
 @main.route('/user/uname>')
 def profile(uname):
@@ -81,7 +131,7 @@ def upvote(id,vote_type):
     print(f'The current vote is {to_str}')
 
     if not votes:
-        new_vote = Votes(vote=vote_type, user_id=current_user.id, pitches_id=id)
+        new_vote = Votes(vote=vote_type, user_id=current_user.id, posts_id=id)
         new_vote.save_vote()
         print('YOU HAVE new VOTED')
 
@@ -90,7 +140,7 @@ def upvote(id,vote_type):
             print('YOU CANNOT VOTE MORE THAN ONCE')
             break
         else:   
-            new_vote = Votes(vote=vote_type, user_id=current_user.id, pitches_id=id)
+            new_vote = Votes(vote=vote_type, user_id=current_user.id, posts_id=id)
             new_vote.save_vote()
             print('YOU HAVE VOTED')
             break
