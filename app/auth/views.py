029d2c21fc97d967221d2973098a39a06134747c
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-16 19:19:07 +0300

    updating the app database

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from .forms import RegistrationForm, LoginForm
 from . import auth
 from ..import db
 from ..models import User
+from ..email import mail_message
 
 
 @auth.route('templates/auth/reqister',methods=['GET','POST'])
@@ -41,7 +42,19 @@ def login():
 
     title = "60sec Pitch|Login"
     return render_template('auth/login.html', login_form = login_form, title = title)
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
+        mail_message("Welcome to Blog Post","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 #logout function
 @auth.route('/logout')
