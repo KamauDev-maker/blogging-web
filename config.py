6bac7f8a7329b699b8c08fbc5cd0c5b5941ccf9a
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-16 16:24:59 +0300

    initializing the application

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:2021@localhost/watchlist'
     
 class ProdConfig(Config):
     
